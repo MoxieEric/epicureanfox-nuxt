39fbad2e773f8a46bc8ea7b37ca43ce912cfedec
Eric Nowels <eric.nowels@moxiesozo.com>
2021-03-28 01:35:39 -0600

    image tag component

---
+++ components/ImageTag.vue
@@ -0,0 +1,19 @@
+<template>
+    <img :src='"/images/fpo-0"+random_number+".jpg"' :alt="image.class || ''" :class="image.class || classes ">
+</template>
+
+<script>
+export default {
+    name: "ImageTag",
+    props: {
+        image: {},
+        classes: ''
+    },
+    data() {
+        let random_number = Math.floor(Math.random(1) * Math.floor(2)) + 1;
+        return {
+            random_number: random_number
+        }
+    }
+}
+</script>
